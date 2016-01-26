b42785479af9fe97c50187af002eee71056e1699
Bruno Simplicio <brunoraphael89@gmail.com>
2016-01-25 22:39:07 -0200

    Exercises of Chapter 9

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,18 +1,17 @@
 class StoreController < ApplicationController
 
-  before_action :how_many, only: [:index]
 
   def index
     @products = Product.order(:title)
+    @count = count
   end
 
   private
 
-    def how_many
-      if session[:counter].nil?
-         session[:counter] = 1
-      else
-         session[:counter] += 1
-      end
+  def count
+    if session[:counter].nil?
+       session[:counter] = 0
     end
+    session[:counter] += 1
+  end
 end
